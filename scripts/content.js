e57072629bbf3cc1d7f9aeda8cc1241d5973ed4c
Abdullah Raza <42661870+AlmostEfficient@users.noreply.github.com>
2022-11-28 03:15:46 +1300

    init

---
+++ scripts/content.js
@@ -0,0 +1,53 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === 'inject') {
+    const { content } = request;
+
+    console.log(content);
+
+    sendResponse({ status: 'success' });
+  }
+});
+
+const insert = (content) => {
+  const elements = document.getElementsByClassName('droid');
+
+  if (elements.length === 0) {
+    return;
+  }
+  
+  const element = elements[0];
+
+  const pToRemove = element.childNodes[0];
+  pToRemove.remove();
+
+  const splitContent = content.split('\n');
+
+  splitContent.forEach((content) => {
+    const p = document.createElement('p');
+
+    if (content === '') {
+      const br = document.createElement('br');
+      p.appendChild(br);
+    } else {
+      p.textContent = content;
+    }
+
+    element.appendChild(p);
+  });
+
+  return true;
+};
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === 'inject') {
+    const { content } = request;
+
+    const result = insert(content);
+
+    if (!result) {
+      sendResponse({ status: 'failed' });
+    }
+
+    sendResponse({ status: 'success' });
+  }
+});
---
+++ scripts/contextMenuServiceWorker.js
@@ -0,0 +1,94 @@
+const getKey = () => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(['openai-key'], (result) => {
+      if (result['openai-key']) {
+        const decodedKey = atob(result['openai-key']);
+        resolve(decodedKey);
+      }
+    });
+  });
+};
+
+const sendMessage = (content) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const activeTab = tabs[0].id;
+
+    chrome.tabs.sendMessage(
+      activeTab,
+      { message: 'inject', content },
+      (response) => {
+        if (response.status === 'failed') {
+          console.log('injection failed.');
+        }
+      }
+    );
+  });
+};
+
+const generate = async (prompt) => {
+  const key = await getKey();
+  const url = 'https://api.openai.com/v1/completions';
+	
+  const completionResponse = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      model: 'text-davinci-002',
+      prompt: prompt,
+      max_tokens: 1250,
+      temperature: 0.7,
+    }),
+  });
+	
+  const completion = await completionResponse.json();
+  return completion.choices.pop();
+}
+
+const generateCompletionAction = async (info) => {
+	try {
+		// Send mesage with generating text (this will be like a loading indicator)
+		sendMessage('generating...');
+	
+    const { selectionText } = info;
+    const basePromptPrefix =
+      `
+			Write me a detailed table of contents for a blog post with the title below.
+			
+			Title:
+			`;
+
+    const baseCompletion = await generate(`${basePromptPrefix}${selectionText}`);
+ 
+		const secondPrompt = 
+		  `
+		  Take the table of contents and title of the blog post below and generate a blog post written in thwe style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
+		
+		  Title: ${selectionText}
+		
+		  Table of Contents: ${baseCompletion.text}
+		
+		  Blog Post:
+		  `;
+		
+    const secondPromptCompletion = await generate(secondPrompt);
+		
+		// Send the output when we're all done
+		sendMessage(secondPromptCompletion.text);
+  } catch (error) {
+    console.log(error);
+
+		// Add this here as well to see if we run into any errors!
+		sendMessage(error.toString());
+  }
+};
+
+chrome.contextMenus.create({
+  id: 'context-run',
+  title: 'Generate blog post',
+  contexts: ['selection'],
+});
+
+chrome.contextMenus.onClicked.addListener(generateCompletionAction);
